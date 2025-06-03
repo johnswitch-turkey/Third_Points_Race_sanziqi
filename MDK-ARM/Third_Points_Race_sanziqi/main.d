7350d6250f30e33e2dabdd1f30949b71d58a54bf
johnswitch-turkey <2309292099@qq.com>
2025-06-03 21:24:22 +0800

    修改代码为状态机

--- MDK-ARM/Third_Points_Race_sanziqi/main.d
+++ MDK-ARM/Third_Points_Race_sanziqi/main.d
@@ -30,4 +30,7 @@ third_points_race_sanziqi/main.o: ..\Core\Src\main.c ..\Core\Inc\main.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_tim_ex.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_uart.h \
   ..\Core\Inc\dma.h ..\Core\Inc\main.h ..\Core\Inc\tim.h \
-  ..\Core\Inc\usart.h ..\Core\Inc\gpio.h
+  ..\Core\Inc\usart.h ..\Core\Inc\gpio.h ..\USER\bsp\bsp_uart.h \
+  D:\Programs\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  D:\Programs\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h \
+  ..\USER\Devices\Emm_V5.h ..\USER\bsp\sys.h ..\USER\bsp\bsp_dwt.h
